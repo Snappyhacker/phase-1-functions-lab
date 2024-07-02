f72ce6315723d58883fe3e19b5d8c6886d876740
“Snappyhacker” <jimodhiambo515@gmail.com>
2024-07-02 12:32:47 +0300

    function

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(block) {
+    const hq = 42;
+    return Math.abs(block - hq);
+  }
+  
+  function distanceFromHqInFeet(block) {
+    const feetPerBlock = 264;
+    return distanceFromHqInBlocks(block) * feetPerBlock;
+  }
+  
+  function distanceTravelledInFeet(start, destination) {
+    const feetPerBlock = 264;
+    return Math.abs(start - destination) * feetPerBlock;
+  }
+  
+  function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+    if (distance <= 400) {
+      return 0;
+    } else if (distance > 400 && distance <= 2000) {
+      return (distance - 400) * 0.02;
+    } else if (distance > 2000 && distance <= 2500) {
+      return 25;
+    } else {
+      return 'cannot travel that far';
+    }
+  }
+  
+  module.exports = {
+    distanceFromHqInBlocks,
+    distanceFromHqInFeet,
+    distanceTravelledInFeet,
+    calculatesFarePrice,
+  };
+  
